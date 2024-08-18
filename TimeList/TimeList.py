f0e2ae395990f635770f5f14249404742e4baddf
huyulun <huyulun@icloud.com>
2024-08-18 17:40:01 +0800

    tlv0.1

--- TimeList/TimeList.py
+++ TimeList/TimeList.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env python
 
-import tkinter as tk
+import tkinter
 from tkinter import messagebox
 import time
 import threading
 
-def show_time():
-    root = tk.Tk()
-    root.withdraw()
+root = tkinter.Tk()
+root.withdraw()
+global orange_data = None
+def load_data():
+    with open('OrangeList.json', 'r') as f:
+        orange_data = json.load(f)
+
+def show_time(task):
     current_time = time.strftime("%H:%M:%S")
-    messagebox.showinfo("时间",f"{current_time}")
-    root.destory()
+    messagebox.showinfo("时间",f"时间：{current_time}\n任务：{task}")
 
 def check_time():
+    if orange_data is None:
+        messagebox.showinfo("错误", "配置文件错误")
+    else:
+        time_list = orange_data.get("TimeList")
+        
     while True:
-        current_time = time.strftime("%H:%M:%S")
-        point_time = "15:00:00"
-        if current_time == point_time:
-            show_time()
-            # 避免同一整点多次弹窗
-            time.sleep(60)
-        # 每秒检查一次
-        time.sleep(1)
+        if isinstance(time_list, list):
+            for time_point in time_list:
+                if time_point.get("TimePoint") == time.strftime("%H:%M:%S"):
+                    task = time_point.get("Task")
+                    show_time(task)
+                    # 避免同一整点多次弹窗
+                    time.sleep(60)
+                #每秒检查一次
+                time.sleep(1)
 
+show_time()
 # 在后台线程中运行检查时间的函数
-threading.Thread(target=check)time, deamon=True).start()
+threading.Thread(target=check_time, daemon=True).start()
 
 # 防止脚本退出
-tk.mainloop()
+root.mainloop()
