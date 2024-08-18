1cbb7d5c1b5de8f1d3113d81d75259f8db75889e
huyulun <huyulun@icloud.com>
2024-08-18 16:09:07 +0800

    tlv0.1

--- TimeList/TimeList.py
+++ TimeList/TimeList.py
@@ -1,19 +1,30 @@
-
+#!/usr/bin/env python
 
 import tkinter as tk
-import tkinter import messagebox
+from tkinter import messagebox
 import time
+import threading
 
-#创建主窗口
-root = tk.Tk()
-#隐藏主窗口
-root.withdraw()
+def show_time():
+    root = tk.Tk()
+    root.withdraw()
+    current_time = time.strftime("%H:%M:%S")
+    messagebox.showinfo("时间",f"{current_time}")
+    root.destory()
 
-#显示弹窗
-messagebox.showinfo("时间表", "现在是？点，现在的任务是？")
+def check_time():
+    while True:
+        current_time = time.strftime("%H:%M:%S")
+        point_time = "15:00:00"
+        if current_time == point_time:
+            show_time()
+            # 避免同一整点多次弹窗
+            time.sleep(60)
+        # 每秒检查一次
+        time.sleep(1)
 
-#延时3秒
-time.sleep(3)
+# 在后台线程中运行检查时间的函数
+threading.Thread(target=check)time, deamon=True).start()
 
-#关闭窗口
-root.destory()
+# 防止脚本退出
+tk.mainloop()
