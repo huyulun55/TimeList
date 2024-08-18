ea4e0426666a692e5f68279b224dffe4ed0f5223
huyulun <huyulun@icloud.com>
2024-08-18 16:45:14 +0800

    tlv0.1

--- TimeList/OrangeList.js
+++ TimeList/OrangeList.js
@@ -1,4 +1,22 @@
 {
-    time: "17:00"
-
+	"TimeList":[
+		{"TimePoint":"8:00:00", "task":"C/C++面试题"}
+		{"TimePoint":"9:00:00", "task":"FREYA挂机"}
+		{"TimePoint":"9:30:00", "task":"STM32练习"}
+		{"TimePoint":"11:00:00", "task":"FREYA挂机"}
+		{"TimePoint":"11:30:00", "task":"中餐"}
+		{"TimePoint":"12:00:00", "task":"动画"}
+		{"TimePoint":"12:30:00", "task":"午觉"}
+		{"TimePoint":"14:00:00", "task":"Linux嵌入式练习"}
+		{"TimePoint":"15:30:00", "task":"休息"}
+		{"TimePoint":"16:00:00", "task":"Linux嵌入式练习"}
+		{"TimePoint":"17:30:00", "task":"晚餐(重启)"}
+		{"TimePoint":"18:00:00", "task":"FREYA挂机"}
+		{"TimePoint":"19:00:00", "task":"板绘"}
+		{"TimePoint":"20:00:00", "task":"水果"}
+		{"TimePoint":"20:30:00", "task":"洗澡"}
+		{"TimePoint":"21:00:00", "task":"板绘"}
+		{"TimePoint":"22:00:00", "task":"洗漱"}
+		{"TimePoint":"22:30:00", "task":"聊天晚安"}
+	]
 }
