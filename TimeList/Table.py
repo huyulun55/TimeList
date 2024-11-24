20bc4a23a511d295f73398a5d0ab73cfefab96b6
huyulun <huyulun@icloud.com>
2024-11-24 22:11:00 +0800

    tlv3.2

--- TimeList/Table.py
+++ TimeList/Table.py
@@ -1,4 +1,15 @@
-import tkinter,sys,time,threading
+"""
+v3.2版本 
+功能：悬浮停靠时间表，支持向上向左向右停靠，根据配置文件现实时间表
+BUG: 向下隐藏有BUG，只要运行select_file函数就会出现，疑似和任务栏有关
+"""
+import os
+import sys
+import json
+import time
+import tkinter
+import threading
+from tkinter import messagebox, simpledialog
 
 # 设置无缓冲区模式方便打印调试
 sys.stdout = open(sys.stdout.fileno(), mode='w', buffering = 1)
@@ -6,24 +17,68 @@ sys.stdout = open(sys.stdout.fileno(), mode='w', buffering = 1)
 # 定义全局变量
 x = 0
 y = 0
-height = 200
-width = 160
+height = 400
+width = 320
 is_hide = None
-
+data = None
 root = tkinter.Tk()
-# 去掉窗口的边框
-root.overrideredirect(True)
-# 设置窗口的透明度为0.8
-root.attributes("-alpha", 0.8)
-# 设置窗口总是位于其他窗口之上
-root.wm_attributes("-topmost", 1)
-# 设置窗口尺寸和初始未知（宽160xp,高200xp,x坐标1375,y坐标200）
-root.geometry(f"{width}x{height}+500+300")
-# 在窗口的左上角放一个标签，显示文本
-tkinter.Label(root, justify="left", font=(None, 12), text="djsf").place(x=0, y=0)
-# 获取屏幕长度
-screen_height = root.winfo_screenheight()
-screen_width = root.winfo_screenwidth()
+screen_height = 0 
+screen_width = 0
+
+# 获取脚本所在的目录
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+def initialize_window():
+    global root, screen_height, screen_width
+    # 隐藏窗口
+    root.withdraw()
+    # 去掉窗口的边框
+    root.overrideredirect(True)
+    # 设置窗口的透明度为0.8
+    root.attributes("-alpha", 0.8)
+    # 设置窗口总是位于其他窗口之上
+    root.wm_attributes("-topmost", 1)
+    # 获取屏幕长度
+    screen_height = root.winfo_screenheight()
+    screen_width = root.winfo_screenwidth()
+    # 设置窗口尺寸和初始未知（宽160xp,高200xp,x坐标1375,y坐标200）
+    root.geometry(f"{width}x{height}+{screen_width-width}+200")
+
+def load_data(filename):
+    global data
+    try:
+        file_path = os.path.join(script_dir, filename)
+        with open(file_path, 'r', encoding = 'utf-8') as f:
+            data = f.readlines()
+        data = ''.join(data)
+    except FileNotFoundError:
+        messagebox.showerror("错误", "配置文件未找到")
+        root.destroy()
+    except json.JSONDecodeError:
+        messagebox.showerror("错误", "配置文件格式错误")
+        root.destroy()
+    except UnicodeDecodeError:
+        messagebox.showerror("错误", "配置文件编码不是UTF-8无法解码")
+        root.destroy()
+
+# 选择配置文件（时间表）
+def select_file():
+    file_choice = simpledialog.askstring("选择时间列表", "请输入希望加载的时间表名称(overtime, bath, leisure, swim):")
+    if file_choice == "overtime":
+        load_data("OvertimeList.txt")
+    elif file_choice == "bath":
+        load_data("BathList.txt")
+    elif file_choice == "leisure":
+        load_data("LeisureList.txt")
+    elif file_choice == "swim":
+        load_data("SwimList.txt")
+    else:
+        messagebox.showerror("错误", "无效的配置文件名")
+        root.destroy()
+    # 显示窗口
+    root.deiconify()
+    # 在窗口的左上角放一个标签，显示文本
+    tkinter.Label(root, justify="left", font=(None, 12), text=data).place(x=0, y=0)
 
 # 记录鼠标点击时的相对位置
 def get_pos(event):
@@ -45,7 +100,7 @@ def change_alpha(event):
     # 鼠标左键双击在两种透明度之间切换
     alpha = root.attributes("-alpha")
     # 更新透明度
-    root.attributes("-alpha", 0.1 if alpha == 0.8 else 0.8)
+    root.attributes("-alpha", 0.3 if alpha == 0.8 else 0.8)
 
 # 关闭窗口
 def close(event):
@@ -64,15 +119,15 @@ def hide_window(step_x, step_y):
         root.geometry(f"{width}x{height}+{current_x + step_x}+{current_y}")
         root.after(10, hide_window, step_x, step_y)
     # 向左隐藏
-    elif is_hide == "left" and current_x > - 140:
+    elif is_hide == "left" and current_x > - width + 20:
         root.geometry(f"{width}x{height}+{current_x - step_x}+{current_y}")
         root.after(10, hide_window, step_x, step_y)
-    # 向下隐藏
+    # 向下隐藏(BUG)
     elif is_hide == "down" and current_y < screen_height - 20:
-        root.geometry(f"{width}x{height}+{current_x}+{current_y + step_y}")
-        root.after(10, hide_window, step_x, step_y)
+         root.geometry(f"{width}x{height}+{current_x}+{current_y + step_y}")
+         root.after(10, hide_window, step_x, step_y)
     # 向上隐藏
-    elif is_hide == "up" and current_y > - 180:
+    elif is_hide == "up" and current_y > - height + 20:
         root.geometry(f"{width}x{height}+{current_x}+{current_y - step_y}")
         root.after(10, hide_window, step_x, step_y)
 
@@ -89,33 +144,32 @@ def show_window(step_x, step_y):
     elif is_hide == "left" and current_x < 0 - 5:
         root.geometry(f"{width}x{height}+{current_x + step_x}+{current_y}")
         root.after(10, show_window, step_x, step_y)
-    # 上隐藏向下显示（5是边缘值）
-    elif is_hide == "down" and current_y > screen_height - height + 5:
-        root.geometry(f"{width}x{height}+{current_x}+{current_y - step_y}")
-        root.after(10, show_window, step_x, step_y)
     # 下隐藏向上显示（5是边缘值）
+    elif is_hide == "down" and current_y > screen_height - height + 5:
+         root.geometry(f"{width}x{height}+{current_x}+{current_y - step_y}")
+         root.after(10, show_window, step_x, step_y)
+    # 上隐藏向下显示（5是边缘值）(BUG)
     elif is_hide == "up" and current_y < 0 - 5:
-        root.geometry(f"{width}x{height}+{current_x}+{current_y + step_y}")
-        root.after(10, show_window, step_x, step_y)
+       root.geometry(f"{width}x{height}+{current_x}+{current_y + step_y}")
+       root.after(10, show_window, step_x, step_y)
 
 def hide_check(event):
     global is_hide
     # 检测隐藏条件
-    if root.winfo_x() >= screen_width - 160 - 40:
+    if root.winfo_x() >= screen_width - width - 1:
         is_hide = "right"
         hide_window(4, 0)
-    elif root.winfo_x() <= 40:
+    elif root.winfo_x() <= 1:
         is_hide = "left"
         hide_window(4, 0)
-    elif root.winfo_y() >= screen_height - 200 - 40:
+    elif root.winfo_y() >= screen_height - height - 1:
         is_hide="down"
         hide_window(0, 4)
-    elif root.winfo_y() <= 40:
+    elif root.winfo_y() <= 1:
         is_hide="up"
         hide_window(0, 4)
     else:
         is_hide = None
-        return
 
 def show_check(event):
     global is_hide
@@ -128,19 +182,20 @@ def show_check(event):
     elif is_hide == "up":
         show_window(0, 4)
 
-# 绑定鼠标左键拖动事件
-root.bind("<B1-Motion>", window_move)
-# 绑定鼠标左键按下事件
-root.bind("<Button-1>", get_pos)
+if __name__ == "__main__":
+    initialize_window()
+    select_file()
+    # 绑定鼠标左键拖动事件
+    root.bind("<B1-Motion>", window_move)
+    # 绑定鼠标左键按下事件
+    root.bind("<Button-1>", get_pos)
+    # 绑定鼠标左键双击切换透明度
+    root.bind("<Double-Button-1>", change_alpha)
+    # 绑定鼠标右键双击关闭窗口
+    root.bind("<Double-Button-3>", close)
+    # 鼠标移出窗口
+    root.bind("<Leave>", hide_check)
+    # 鼠标移入窗口
+    root.bind("<Enter>", show_check)
 
-# 绑定鼠标左键双击切换透明度
-root.bind("<Double-Button-1>", change_alpha)
-# 绑定鼠标右键双击关闭窗口
-root.bind("<Double-Button-3>", close)
-
-# 鼠标移出窗口
-root.bind("<Leave>", hide_check)
-# 鼠标移入窗口
-root.bind("<Enter>", show_check)
-
-root.mainloop()
+    root.mainloop()
